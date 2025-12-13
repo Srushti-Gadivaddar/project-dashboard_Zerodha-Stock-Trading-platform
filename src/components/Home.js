abdef2882abe86a2c7758dee2b6b9758c1020848
Srushti-Gadivaddar <gadivaddarsrushti@gmail.com>
2025-12-13 10:48:43 +0530

    Added The Dashboard

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TopBar from "./TopBar";
+import Dashboard from './dashboard';
+
+const Home = () => {
+  return (
+    <>
+      <TopBar />
+      <Dashboard/>
+    </>
+  );
+};
+
+export default Home;
